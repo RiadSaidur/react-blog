53c4ebb98dc5f84f9b0205c2422d683a2ab77c8d
deathcrush <reidvai1998@gmail.com>
2019-12-24 12:55:41 +0600

    updated: linked with User view and basic styling

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,25 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './stylesheets/Posts.css'
+
 function NewPost({ posts, upvote, downvote }){
   return(
     <div className="posts">
       {
         posts.map(post => (
-          <Link to={`/comments/${post.key}`}>
-            <div key={post.key} className="post" >
-              <h3>{post.title}</h3>
-              <p>{post.msg}</p>
-              <div>
+          
+            <div className="post" key={post.key}>
+              <Link to={`/${post.author}`}>
+                <p className="post-author">{post.author}</p>
+              </Link>
+              <Link to={`/comments/${post.key}`}>
+                <h3>{post.title}</h3>
+                <p>{post.msg}</p>
+              </Link>
+              <div className="post-footer">
                 <p>{post.likes}</p>
-                <button onClick={() => upvote(post.key)}>Upvote</button>
-                <button
-                  onClick={() => downvote(post.key)}
-                  className={post.likes<=0 ? 'hidden' : ''}
-                >Downvote</button>
+                <div className="post-footer-cta">
+                  <button className="post-cta" onClick={() => upvote(post.key)}>Upvote</button>
+                  <button
+                    onClick={() => downvote(post.key)}
+                    className={post.likes<=0 ? 'hidden' : 'post-cta'}
+                  >Downvote</button>
+                </div>
               </div>
             </div>
-          </Link>
         ))
       }
     </div>
