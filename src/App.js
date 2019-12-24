e1162c1e212acd4d0b3a594f4e751f557d806163
deathcrush <reidvai1998@gmail.com>
2019-12-24 12:51:56 +0600

    added: routes and global styling

--- src/App.js
+++ src/App.js
@@ -8,22 +8,19 @@ import Comments from './components/Comments';
 import Signin from './components/Signin';
 import Signup from './components/Signup';
 import About from './components/About';
+import User from './components/User';
 
 function App(){
   
   return(
     <Router>
       <Header />
-      {/* <div className='container'>
-        <Header />
-        <NewPost newPost={newPost} />
-        <Posts posts={posts} upvote={upvote} downvote={downvote}/>
-      </div> */}
       <Switch>
         <Route path='/' exact component={Home} />
         <Route path='/signin' exact component={Signin} />
         <Route path='/signup' exact component={Signup} />
         <Route path='/about' exact component={About} />
+        <Route path='/:user' exact component={User} />
         <Route path='/comments/:id' exact component={Comments} />
       </Switch>
     </Router>
