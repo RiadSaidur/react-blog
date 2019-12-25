416879e34af68f247e079fa1484ca9cdf2d63407
deathcrush <reidvai1998@gmail.com>
2019-12-25 22:02:40 +0600

    added: basic signin/signup page

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
 function Signin(){
+  const formHandler = event => {
+    event.preventDefault();
+    event.persist();
+    const user = {
+      email: event.target.elements[0].value,
+      password: event.target.elements[1].value
+    }
+    console.table(user);
+  };
   return(
-    <h1>Signin</h1>
+    <div className="signIn-container">
+      <button>Sign Up</button>
+      <h1>Sign In</h1>
+      <form onSubmit={formHandler}>
+        <input type="email" placeholder="Email"/>
+        <input type="password" placeholder="Password"/>
+        <input type="submit" value="Sign In"/>
+      </form>
+      <p>Forgot Password?</p>
+      <p>Don't have an account? Sign Up now!</p>
+    </div>
   )
 }
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
 function Signin(){
+  const formHandler = event => {
+    event.preventDefault();
+    event.persist();
+    const user = {
+      email: event.target.elements[0].value,
+      password: event.target.elements[1].value,
+      repeatPassword: event.target.elements[2].value,
+      dob: event.target.elements[3].value
+    }
+    if(user.password !== user.repeatPassword) return alert('You dumb fuck');
+    console.table(user);
+  };
   return(
-    <h1>Signup</h1>
+    <div className="signUp-container">
+      <button>Sign In</button>
+      <h1>Sign Up</h1>
+      <form onSubmit={formHandler}>
+        <input type="email" placeholder="Email"/>
+        <input type="password" placeholder="Password"/>
+        <input type="password" placeholder="Repeat Password"/>
+        <input type="date"/>
+        <input type="submit" value="Sign In"/>
+      </form>
+      <p>Alredy have an account? Sign In instead!</p>
+    </div>
   )
 }
 
