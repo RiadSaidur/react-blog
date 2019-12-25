d5972eb0a06170b654cfb27f061b91d158fdeca4
deathcrush <reidvai1998@gmail.com>
2019-12-25 22:00:53 +0600

    added: route to editing post/comment

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Signin from './components/Signin';
 import Signup from './components/Signup';
 import About from './components/About';
 import User from './components/User';
+import Edit from './components/Edit';
 
 function App(){
   
@@ -21,6 +22,7 @@ function App(){
         <Route path='/signup' exact component={Signup} />
         <Route path='/about' exact component={About} />
         <Route path='/:user' exact component={User} />
+        <Route path='/edit/:type/:id' exact component={Edit} />
         <Route path='/comments/:id' exact component={Comments} />
       </Switch>
     </Router>
