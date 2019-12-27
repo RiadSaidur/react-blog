f376e74efe701757b7481413737705e5f8b0c29d
deathcrush <reidvai1998@gmail.com>
2019-12-27 14:49:31 +0600

    added: view post by tags

---
+++ src/components/TaggedPost.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+import Posts from './Posts';
+
+export default function TaggedPost({ match }) {
+  const [posts, updatePosts] =  useState([
+    {
+      key: 0,
+      author: 'AfricanBoi51',
+      title: 'Title',
+      msg: 'Msg',
+      likes: 10,
+      tags: ['react', 'dom', 'one']
+    },
+    {
+      key: 1,
+      author: 'DudeBoi',
+      title: "Okay",
+      msg: "Okay bro",
+      likes: 5,
+      tags: ['react', 'dom', 'two']
+    }
+  ]);
+  const upvote = key => {
+    let updates = posts;
+    updates[key].likes++;
+    updatePosts([...updates]);
+  };
+  const downvote = key => {
+    let updates = posts;
+    updates[key].likes--;
+    updatePosts([...updates]);
+  }
+  return (
+    <div className="tagged-container">
+      <h4>p/{ match.params.tag }</h4>
+      <Posts posts={posts} upvote={upvote} downvote={downvote}/>
+    </div>
+  )
+}
