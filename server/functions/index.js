cbae3dfa0b7214deecf43fdfc4311d8616a28b6c
deathcrush <reidvai1998@gmail.com>
2020-01-09 02:36:49 +0600

    created: api routes for comments and newcomment

--- server/functions/index.js
+++ server/functions/index.js
@@ -15,4 +15,22 @@ app.get('/posts', async (req, res) => {
   return res.json(posts);
 });
 
+app.post('/comments', async (req, res) => {
+  const id = req.body.id;
+  const docs = await db.collection('comments').doc(id).get();
+  return res.json(docs.data());
+});
+
+app.post('/newcommment', async (req, res) => {
+  const id = req.body.id;
+  const comment = {
+    key: Math.floor(Math.random()*10000),
+    user: req.body.user,
+    msg: req.body.msg,
+    createdAt: new Date().toISOString()
+  };
+  await db.collection('comments').doc(id).update({ cmnts: admin.firestore.FieldValue.arrayUnion(comment)});
+  return res.status(201).json({ message: "comment successfully added"});
+});
+
 exports.api = functions.region('asia-east2').https.onRequest(app);
