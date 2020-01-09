272d65f4387050f8fe40e7f97af9b8a8585bbdc3
deathcrush <reidvai1998@gmail.com>
2020-01-09 12:38:49 +0600

    added: updating comment route

--- server/functions/index.js
+++ server/functions/index.js
@@ -15,14 +15,14 @@ app.get('/posts', async (req, res) => {
   return res.json(posts);
 });
 
-app.post('/comments', async (req, res) => {
-  const id = req.body.id;
+app.get('/comments/:id', async (req, res) => {
+  const id = req.params.id;
   const docs = await db.collection('comments').doc(id).get();
   return res.json(docs.data());
 });
 
-app.post('/newcommment', async (req, res) => {
-  const id = req.body.id;
+app.post('/newcommment/:id', async (req, res) => {
+  const id = req.params.id;
   const comment = {
     key: Math.floor(Math.random()*10000),
     user: req.body.user,
@@ -31,6 +31,19 @@ app.post('/newcommment', async (req, res) => {
   };
   await db.collection('comments').doc(id).update({ cmnts: admin.firestore.FieldValue.arrayUnion(comment)});
   return res.status(201).json({ message: "comment successfully added"});
+});
+
+app.patch('/editcomment/:id/:key', async (req, res) => {
+  const id = req.params.id;
+  const key = parseInt(req.params.key);
+  const docs = await db.collection('comments').doc(id).get();
+  let comments = docs.data().cmnts;
+  const idx = comments.findIndex(comment => {
+    return comment.key === key;
+  });
+  comments[idx].msg = req.body.msg;
+  await db.collection('comments').doc(id).set({ cmnts: comments });
+  return res.status(201).json({ message: "comment successfully edited"});
 });
 
 exports.api = functions.region('asia-east2').https.onRequest(app);
