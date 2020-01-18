652d505c9f1ef0750ce4a7f207791175337f8cf5
deathcrush <reidvai1998@gmail.com>
2020-01-19 01:46:09 +0600

    added: signup and signin methods using store

--- client/src/views/Signin.js
+++ client/src/views/Signin.js
@@ -2,36 +2,41 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import '../stylesheets/Signin.css'
+import UserContext from '../store/userContext/UserContext';
 
 function Signin(){
-  const formHandler = event => {
-    event.preventDefault();
-    event.persist();
-    const user = {
-      email: event.target.elements[0].value,
-      password: event.target.elements[1].value
-    }
-    console.table(user);
-  };
   return(
-    <div className="sign-container">
-      <div className="contents nu-elevate-card">
-        <h2>Sign In</h2>
-        <div className="form">
-          <form onSubmit={formHandler}>
-            <input type="email" placeholder="Email" className="nu-elevate-cta"/>
-            <input type="password" placeholder="Password" className="nu-elevate-cta"/>
-            <input type="submit" value="Sign In" className="nu-elevate-cta cta"/>
-          </form>
-          <div className="sign-options">
-            <p>Forgot Password?</p>
-            <p>
-              Don't have an account? <Link to="/signup">Sign Up</Link>
-            </p>
+    <UserContext.Consumer>{ ({ signIn }) => {
+      const formHandler = event => {
+        event.preventDefault();
+        event.persist();
+        const creds = {
+          email: event.target.elements[0].value,
+          password: event.target.elements[1].value
+        }
+        signIn(creds);
+      };
+      return (
+        <div className="sign-container">
+          <div className="contents nu-elevate-card">
+            <h2>Sign In</h2>
+            <div className="form">
+              <form onSubmit={formHandler}>
+                <input type="email" placeholder="Email" className="nu-elevate-cta"/>
+                <input type="password" placeholder="Password" className="nu-elevate-cta"/>
+                <input type="submit" value="Sign In" className="nu-elevate-cta cta"/>
+              </form>
+              <div className="sign-options">
+                <p>Forgot Password?</p>
+                <p>
+                  Don't have an account? <Link to="/signup">Sign Up</Link>
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      )
+    }}</UserContext.Consumer>
   )
 }
 
--- client/src/views/Signup.js
+++ client/src/views/Signup.js
@@ -1,30 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import UserContext from '../store/userContext/UserContext';
 
 function Signin(){
-  const formHandler = event => {
-    event.preventDefault();
-    event.persist();
-    const user = {
-      email: event.target.elements[0].value,
-      password: event.target.elements[1].value,
-      repeatPassword: event.target.elements[2].value,
-      dob: event.target.elements[3].value
-    }
-    if(user.password !== user.repeatPassword) return alert('You dumb fuck');
-    console.table(user);
-  };
   return(
-    <div className="sign-container nu-elevate-card contents">
-      <h2>Sign Up</h2>
-      <form onSubmit={formHandler} className="form">
-        <input type="email" placeholder="Email" className="nu-elevate-card"/>
-        <input type="password" placeholder="Password" className="nu-elevate-card"/>
-        <input type="password" placeholder="Repeat Password" className="nu-elevate-card"/>
-        <input type="text" placeholder="user handle" className="nu-elevate-card"/>
-        <input type="submit" value="Sign In" className="nu-elevate-card cta"/>
-      </form>
-      <p className="sign-options">Alredy have an account? Sign In instead!</p>
-    </div>
+    <UserContext.Consumer>{ ({ signUp }) => {
+      const formHandler = event => {
+        event.preventDefault();
+        event.persist();
+        const creds = {
+          handle: event.target.elements[0].value,
+          email: event.target.elements[1].value,
+          password: event.target.elements[2].value,
+          repeatPassword: event.target.elements[3].value
+        }
+        signUp(creds);
+      };
+      return (
+        <div className="sign-container nu-elevate-card contents">
+          <h2>Sign Up</h2>
+          <form onSubmit={formHandler} className="form">
+          <input type="text" placeholder="user handle" className="nu-elevate-card"/>
+            <input type="email" placeholder="Email" className="nu-elevate-card"/>
+            <input type="password" placeholder="Password" className="nu-elevate-card"/>
+            <input type="password" placeholder="Repeat Password" className="nu-elevate-card"/>
+            <input type="submit" value="Sign Up" className="nu-elevate-card cta"/>
+          </form>
+          <p className="sign-options">Alredy have an account? <Link to="/signin">Sign In</Link> instead!</p>
+        </div>
+      )
+    }}</UserContext.Consumer>
   )
 }
 
