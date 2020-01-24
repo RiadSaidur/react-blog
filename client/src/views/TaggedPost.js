527f2ea016c39d40645b4c7f2ab2c92e70d18a30
deathcrush <reidvai1998@gmail.com>
2020-01-24 17:56:26 +0600

    added: PostsByUser and PostsByTag

--- client/src/views/TaggedPost.js
+++ client/src/views/TaggedPost.js
@@ -1,71 +1,16 @@
-import React, { useState } from 'react'
+import React, { useContext , useEffect} from 'react';
 
 import Posts from '../components/Posts';
+import PostContext from '../store/postContext/PostContext';
 
 export default function TaggedPost({ match }) {
-  const [posts, updatePosts] =  useState([
-    {
-      id: 'qK8gorBgKHc4ITmQEcrk',
-      tags: [ 'delete', 'test', 'react' ],
-      createdAt: '2020-01-12T19:33:35.149Z',
-      counts: 6,
-      title: 'still authorized',
-      msg: 'this is epic',
-      author: 'boomer',
-      likes: 0,
-      downvotes: [],
-      upvotes: [ 'boomer' ]
-    },
-    {
-      id: 'b4t4w5SnJ22FVZiXFL8p',
-      likes: 0,
-      tags: [ 'react', 'song' ],
-      createdAt: '2020-01-10T04:05:51.429Z',
-      counts: 1,
-      title: 'this post was made from an api',
-      msg: 'I am very exited and stupid',
-      author: 'saidur',
-      cmntId: 'pey92CA3jpmth6p1XajW',
-      downvotes: [],
-      upvotes: [ 'boomer' ]
-    },
-    {
-      id: 'yjRPsY4rnFVLFB6pqjAB',
-      title: 'First Post',
-      msg: 'So familiar and overwhelmingly pure',
-      author: 'saidur',
-      cmntId: 'UHRd23B3PQfSfnUVL0jG',
-      likes: 8,
-      tags: [ 'song', 'lyric', 'tool' ],
-      createdAt: '2020-01-12T19:33:35.149Z',
-      counts: 0,
-      downvotes: [ "boomer" ],
-      upvotes: []
-    },
-    {
-      id: 'g2lGLvJ3ZOwOeNb7SeQd',
-      tags: [ 'stupid', 'pixxel', 'tool', 'react' ],
-      createdAt: '2020-01-12T19:33:35.149Z',
-      counts: 2,
-      title: 'this is a dummy me',
-      msg: 'edited my post again, bruh',
-      author: 'rakib',
-      cmntId: 'NP0GV8J9u8XD1d9dKhsu',
-      likes: 2,
-      downvotes: [ "boomer" ],
-      upvotes: []
-    }
-  ]);
-  const upvote = key => {
-    let updates = posts;
-    updates[key].likes++;
-    updatePosts([...updates]);
-  };
-  const downvote = key => {
-    let updates = posts;
-    updates[key].likes--;
-    updatePosts([...updates]);
-  }
+  const { posts, setPostsByTag, upvote, downvote } = useContext(PostContext);
+
+  useEffect(() => {
+    setPostsByTag(match.params.tag);
+    // eslint-disable-next-line
+  }, []);
+  
   return (
     <div className="tagged-container">
       <h2>p/{ match.params.tag }</h2>
